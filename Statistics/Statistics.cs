42f8fc898cbd8e7ac45b75a89a070d1737534601
PseudoNuke <thilakashwin10@gmail.com>
2020-09-07 12:27:29 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,20 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public Stats CalculateStatistics(List<double> numbers)
+        {
+            Stats obj = new Stats();
+            if (numbers.Count > 0) {
+                obj.max = numbers.Max();
+                obj.min = numbers.Min();
+                obj.average = numbers.Average();
+            }
+            else {
+                obj.max = double.NaN;
+                obj.min = double.NaN;
+                obj.average = double.NaN;
+            }
+            return obj;
         }
     }
 }
